54275fe2e6b39c53cf3fd3083e8689707aeafffa
omar-amrouss <omar.amrouss@outlook.be>
2024-03-17 23:15:59 +0000

    first commit

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Task;
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $tasks = Task::all();
+        return view('tasks.index', compact('tasks'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('tasks.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'title' => 'required|max:100',
+            'detail' => 'required|max:500',
+            ]);
+            $task = new Task;
+            $task->title = $request->title;
+            $task->detail = $request->detail;
+            $task->save();
+            return back()->with('message', "La tâche a bien été créée !");
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Task $task)
+    {
+        return view('tasks.show', compact('task'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Task $task)
+    {
+        return view('tasks.edit', compact('task'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Task $task)
+    {
+        $data = $request->validate([
+            'title' => 'required|max:100',
+            'detail' => 'required|max:500',
+            ]);
+            $task->title = $request->title;
+            $task->detail = $request->detail;
+            $task->state = $request->has('state');
+            $task->save();
+            return back()->with('message', "La tâche a bien été modifiée !");
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Task $task)
+    {
+        $task->delete();
+        return back();
+    }
+}
